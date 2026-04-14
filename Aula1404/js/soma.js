a213d041121cc7c472f3e6900fe00a03c36f82cf
Bruno <brunicacio28@gmail.com>
2026-04-14 08:21:29 -0300

    Aula 14/04 sobre HTML, CSS e JS

---
+++ Aula1404/js/soma.js
@@ -0,0 +1,6 @@
+var number1 = parseInt(prompt("Digite o primeiro numero: "));
+var number2 = parseInt(prompt("Digite o segundo numero: "));
+
+var soma = number1 + number2;
+
+console.log("A soma é ",soma);
