b5dbaef60be7f386de73a317d5a529363671c193
Bruno <brunicacio28@gmail.com>
2026-05-11 23:02:34 -0300

    Exercicos com HTML e JS

--- Aula2804/Verificar Idade/js/script.js
+++ Aula2804/Verificar Idade/js/script.js
@@ -1,9 +1,18 @@
-var idade = document.querySelector("#n1");
+var idadeInput = document.querySelector("#idadeInput");
+var resultado = document.querySelector("#resultado")
 
-if (idade < 18) {
-    console.log("Você é menor de idade")
+function verificar(){
+    var idade = parseInt(idadeInput.value); 
+    if(isNaN(idade)) {
+        resultado.innerHTML="Digite sua idade";
+        return;
+    }
+    if(idade < 18) {
+            console.log("Você é menor de idade")
+            resultado.innerHTML="Você é menor de idade";
+        }
+    else {
+            console.log("Você é maior de idade")
+            resultado.innerHTML="Você é maior de idade";
+        }   
 }
-else {
-    console.log("Você é maior de idade")
-}
-
