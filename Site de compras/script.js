4d8c71f15e063d5ec3c67567e4a1aa05ee58f5f0
Bruno <brunicacio28@gmail.com>
2026-06-05 00:03:05 -0300

    Criação do sistema de adicionar/remover produtos com soma

--- Site de compras/script.js
+++ Site de compras/script.js
@@ -1,15 +1,19 @@
 var subtotal = document.querySelector(".subtotal");
-var botaomenos = document.querySelector(".botaomenos")
 
 function adicionar(botao) {
     var trtable = botao.parentNode.parentNode.parentNode;
     var valor = trtable.querySelector(".valor");
     var total = trtable.querySelector(".total");
     var qntd = trtable.querySelector(".qntd");
+    var botaomenos = trtable.querySelector(".botaoretirar");
 
     qntd.innerHTML = (Number(qntd.innerHTML || 0)) + 1;
     total.innerHTML = (Number(total.innerHTML)) + (Number(valor.innerHTML));
     subtotal.innerHTML = (Number(subtotal.innerHTML)) + (Number(valor.innerHTML));
+    
+    if (qntd.innerHTML > 0) {
+        botaomenos.classList.remove("inativo");
+    }
 }
 
 function retirar(botao) {
@@ -17,11 +21,15 @@ function retirar(botao) {
     var valor = trtable.querySelector(".valor");
     var total = trtable.querySelector(".total");
     var qntd = trtable.querySelector(".qntd");
+    var botaomenos = trtable.querySelector(".botaoretirar");
 
     if (total.innerHTML > 0) {
         qntd.innerHTML = (Number(qntd.innerHTML || 0)) - 1;
         total.innerHTML = (Number(total.innerHTML || 0)) - (Number(valor.innerHTML));   
         subtotal.innerHTML = (Number(subtotal.innerHTML)) - (Number(valor.innerHTML));
+    }
+    if (qntd.innerHTML <= 0) {
+        botaomenos.classList.add("inativo");
     }
 }
 
