0637b90e5378f2bdce2eb69543eb23094c97bda9
Bruno <brunicacio28@gmail.com>
2026-06-16 08:37:02 -0300

    Criação do sistema de adicionar/remover produtos com soma

---
+++ Site de compras/script-iphone.js
@@ -0,0 +1,7 @@
+function abrirCarrinho() {
+    document.querySelector(".carrinho").classList.add("aberto")
+}
+
+function fecharCarrinho() {
+    document.querySelector(".carrinho").classList.remove("aberto")
+}
